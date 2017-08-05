ec697043c9a9897e869aca5eed413d25e762434a
xuxinming <1042407818@qq.com>
2017-08-05 15:02:35 +0800

    metaInfo更新

--- commonservice/src/main/java/com/naruto/mobile/framework/MetaInfo.java
+++ commonservice/src/main/java/com/naruto/mobile/framework/MetaInfo.java
@@ -2,8 +2,12 @@ package com.naruto.mobile.framework;
 
 import com.naruto.mobile.base.serviceaop.service.BaseMetaInfo;
 import com.naruto.mobile.base.serviceaop.service.ServiceDescription;
-import com.naruto.mobile.framework.biz.ext.ShortCutService;
-import com.naruto.mobile.framework.biz.ext.impl.ShortCutServiceImpl;
+import com.naruto.mobile.framework.biz.ext.photo_demo.service.PhotoService;
+import com.naruto.mobile.framework.biz.ext.photo_demo.service.impl.PhotoServiceImpl;
+import com.naruto.mobile.framework.biz.ext.shortCut.ShortCutService;
+import com.naruto.mobile.framework.biz.ext.shortCut.impl.ShortCutServiceImpl;
+import com.naruto.mobile.framework.service.common.multimedia.api.MultimediaImageService;
+import com.naruto.mobile.framework.service.common.multimediabiz.biz.src.com.alipay.android.phone.mobilecommon.multimediabiz.biz.service.MultimediaImageServiceImpl;
 
 public class MetaInfo extends BaseMetaInfo{
 
@@ -14,6 +18,21 @@ public class MetaInfo extends BaseMetaInfo{
         shortcutServiceDescription.setInterfaceClass(ShortCutService.class.getName());
         shortcutServiceDescription.setLazy(true);
         addService(shortcutServiceDescription);
+
+        //测试Activity A 跳转到业务独立的Activity B上，并在B上进行交互操作后，将数据返回给A
+        ServiceDescription photoServiceServiceDescription = new ServiceDescription();
+        photoServiceServiceDescription.setName("photoServiceService");
+        photoServiceServiceDescription.setClassName(PhotoServiceImpl.class.getName());
+        photoServiceServiceDescription.setInterfaceClass(PhotoService.class.getName());
+        photoServiceServiceDescription.setLazy(true);
+        addService(photoServiceServiceDescription);
+
+        ServiceDescription multimiaServiceDescription = new ServiceDescription();
+        shortcutServiceDescription.setName("multimiaService");
+        shortcutServiceDescription.setClassName(MultimediaImageServiceImpl.class.getName());
+        shortcutServiceDescription.setInterfaceClass(MultimediaImageService.class.getName());
+        shortcutServiceDescription.setLazy(true);
+        addService(multimiaServiceDescription);
     }
 
 }
